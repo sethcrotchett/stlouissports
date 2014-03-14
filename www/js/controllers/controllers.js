f89f7dc24e6988739ba59270e78be00d57dfd1a6
Seth <sethcrotchett@gmail.com>
2014-03-14 15:18:09 -0500

    cleanup and added news controllers

--- www/js/controllers/controllers.js
+++
@@ -1,8 +0,0 @@
-angular.module('stlsports.controllers', [])
-
-
-// A simple controller that shows a tapped item's data
-.controller('PetDetailCtrl', function($scope, $stateParams, PetService) {
-  // "Pets" is a service returning mock data (services.js)
-  $scope.pet = PetService.get($stateParams.petId);
-});

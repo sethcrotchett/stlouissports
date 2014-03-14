475878f1deb3d372b31e13d3bfc56fe0549b3e47
Seth <sethcrotchett@gmail.com>
2014-03-14 13:55:35 -0500

    cleanup

--- www/js/controllers/team-detail-controller.js
+++ www/js/controllers/team-detail-controller.js
@@ -9,31 +9,4 @@ angular.module('stlsports.controllers')
 	$scope.sport = $stateParams.sport;
 	$scope.league = $stateParams.league;
 	$scope.teamId = $stateParams.teamId;
-
-	  
-//	  $scope.team;
-//	  $scope.teamnews;
-//	  $scope.status;
-//	  getTeamNews();
-//  
-//	  function getTeamNews(){
-//		    // Show the loading overlay and text
-//		    $scope.loading = $ionicLoading.show({
-//		      content: 'Loading',
-//		      animation: 'fade-in',
-//		      showBackdrop: true,
-//		      maxWidth: 200,
-//		      showDelay: 500
-//		    });		  
-//		  
-//		  TeamNewsService.get($stateParams.sport, $stateParams.league, $stateParams.teamId)
-//              .success(function (data) {
-//            	 $scope.loading.hide();
-//                 $scope.teamnews = data.headlines;
-//              })
-//              .error(function (error) {
-//            	  $scope.loading.hide();
-//                  $scope.status = 'Unable to load team news: ' + error.message;
-//              });
-//	  }
 });
